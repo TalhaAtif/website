1bd2817af98da50d4739fcc55f94a31f7713baf1
_tvd <talha.atif.89@gmail.com>
2024-09-03 15:13:08 -0400

    Updates Card Info

--- scriptnew.js
+++ scriptnew.js
@@ -4,11 +4,12 @@ let navLinks = document.querySelectorAll("nav ul li a");
 window.onscroll = () => {
   sections.forEach((sec) => {
     let top = window.scrollY;
-    let offset = sec.offsetTop;
+    let offset = sec.getBoundingClientRect().top;
     let height = sec.offsetHeight;
     let id = sec.getAttribute("id");
 
-    if (top >= offset - height * 0.5 && top < offset + height * 0.5) {
+    // Calculate the trigger point when the section occupies at least half of the viewport height
+    if (offset <= window.innerHeight / 2 && offset + height >= window.innerHeight / 2) {
       navLinks.forEach((links) => {
         links.classList.remove("active");
         document
